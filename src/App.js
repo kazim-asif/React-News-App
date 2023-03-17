bb60be2f8c9510ec9800a329b4a162afe84e5b36
Kazim786-dev <kazimasif2020@gmail.com>
2023-03-17 12:20:30 +0500

    set url for gh-pages

--- src/App.js
+++ src/App.js
@@ -9,17 +9,17 @@ function App() {
 
   return (
     <div className='App'>
-      <BrowserRouter basename='React-News-App'>
-      {/* <BrowserRouter> */}
+      {/* <BrowserRouter basename='React-News-App'> */}
+      <BrowserRouter>
         <Routes>
-          <Route exact path='/' element={<LandingPagecomp category='general'/>}></Route>
-          <Route path='/general' element={<LandingPagecomp category='general'/>}></Route>
-          <Route path='/sports' element={<LandingPagecomp category='sports'/>}></Route>
-          <Route path="/business" element={<LandingPagecomp category="business" />}></Route>
-          <Route path="/entertainment" element={<LandingPagecomp category="entertainment" />}></Route>
-          <Route path="/health" element={<LandingPagecomp category="health" />}></Route>
-          <Route path="/science" element={<LandingPagecomp category="science" />}></Route>
-          <Route path="/technology" element={<LandingPagecomp category="technology" />}></Route>
+          <Route exact path='/React-News-App/' element={<LandingPagecomp category='general'/>}></Route>
+          <Route path='/React-News-App/general' element={<LandingPagecomp category='general'/>}></Route>
+          <Route path='/React-News-App/sports' element={<LandingPagecomp category='sports'/>}></Route>
+          <Route path="/React-News-App/business" element={<LandingPagecomp category="business" />}></Route>
+          <Route path="/React-News-App/entertainment" element={<LandingPagecomp category="entertainment" />}></Route>
+          <Route path="/React-News-App/health" element={<LandingPagecomp category="health" />}></Route>
+          <Route path="/React-News-App/science" element={<LandingPagecomp category="science" />}></Route>
+          <Route path="/React-News-App/technology" element={<LandingPagecomp category="technology" />}></Route>
         </Routes>
       </BrowserRouter>
     </div>
