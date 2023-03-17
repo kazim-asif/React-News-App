c6bae86f19163a553cc519aa72ada5d97cd830bc
Kazim786-dev <kazimasif2020@gmail.com>
2023-03-17 05:22:47 +0500

    set for production

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
 
   return (
     <div className='App'>
-      <BrowserRouter basename={window.location.pathname || ''}>
+      <BrowserRouter basename={process.env.PUBLIC_URL + '/'}>
       {/* <BrowserRouter> */}
         <Routes>
           <Route exact path='/' element={<LandingPagecomp category='general'/>}></Route>
